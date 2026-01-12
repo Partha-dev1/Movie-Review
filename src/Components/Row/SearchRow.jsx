0cdcc8e2ceacfb6581b5657aadfc7693d3fb0099
Partha-dev1 <psb143978@gmail.com>
2026-01-12 12:14:15 +0530

    Update SearchRow.jsx

--- src/Components/Row/SearchRow.jsx
+++ src/Components/Row/SearchRow.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import './Row.css'
+import { useNavigate } from 'react-router-dom'
 
 const SearchRow = ({ searchList, title }) => {
+  const navigate = useNavigate()
+
+  const handleClick = (id) => {
+    navigate(`/movie/${id}`)
+  }
+
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="row">
+      <h2>{title}</h2>
+
       <div className="movie-container">
         {searchList && searchList.length > 0 ? (
           searchList.map((item) => (
-            <img
-              key={item.id}
-              src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
-              height="200px"
-              width="300px"
-              alt={item.title || 'Movie poster'}
-            />
+            item.backdrop_path && (
+              <img
+                key={item.id}
+                className="movie-poster"
+                src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
+                height="200px"
+                width="300px"
+                alt={item.title || 'Movie poster'}
+                onClick={() => handleClick(item.id)}
+              />
+            )
           ))
         ) : (
           <p>No movies found...</p>
